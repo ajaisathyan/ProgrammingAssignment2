c8cf34e3c2c836f87ca3e266394bb47b7876b5ce
Ajai Sathyan <ajaisathyan@gmail.com>
2017-04-09 20:11:35 +0530

    Comments have been updated to match the problem statement.

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,8 @@
 ## used to create a special object that stores a matrix and 
 ## caches its inverse.
 
-## makeCacheMatrix creates a special matrix, which is really 
-## a list containing a function to 
+## makeCacheMatrix creates a special "matrix" object that can cache 
+## its inverse. It is really a list containing functions to 
 ## set the value of the matrix
 ## get the value of the matrix
 ## set the value of the inverse
@@ -26,10 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve function calculates the inverse of the special 
 ## "matrix" created with the makeCacheMatrix function. However, 
-## it first checks to see if the inverse has already been calculated. 
-## If so, it gets the inverse from the cache and skips the computation. 
-## Otherwise, it calculates the inverse of the data and sets the 
-## value of the inverse in the cache via the setinverse function.
+## it first checks to see if the inverse has already been calculated 
+## (and the matrix has not changed). If so, it gets the inverse 
+## from the cache and skips the computation. Otherwise, it calculates 
+## the inverse of the data and sets the value of the inverse in the 
+## cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         inverse <- x$getinverse()
